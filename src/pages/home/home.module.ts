bd9a4bcc21730b5ddceea738b38cd5447dbcdaaf
Luan Reis <luanreis2202@gmail.com>
2022-03-25 20:01:38 -0300

    Lazy loading

---
+++ src/pages/home/home.module.ts
@@ -0,0 +1,13 @@
+import { IonicPageModule } from 'ionic-angular/module';
+import { NgModule } from '@angular/core';
+import { HomePage } from './home';
+
+@NgModule({
+
+    declarations: [HomePage],
+    imports: [IonicPageModule.forChild(HomePage)]
+
+})
+
+export class HomeModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
