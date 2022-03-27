8bddf0ee1683c1970e3c091f3f862fc350bb5411
Luan Reis <luanreis2202@gmail.com>
2022-03-27 18:15:14 -0300

    Mostrando as categorias

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategorieDTO } from '../../models/categories.dto';
 import { CategorieService } from '../../services/domain/categories.service';
 
 /**
@@ -16,6 +18,10 @@ import { CategorieService } from '../../services/domain/categories.service';
 })
 export class CategoriesPage {
 
+  bucketBaseUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategorieDTO[];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -27,9 +33,12 @@ export class CategoriesPage {
   ionViewDidLoad() {
     this.categorieService.findAll()
     .subscribe(response => {
-            console.log(response);
+      this.items = response;
+    },
+    error =>{
+      console.log(error);
+
     });
-    
-  }
+   }
 
 }
