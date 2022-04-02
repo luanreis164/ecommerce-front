4d6ecc7248d2d1d8d9390266205d1030a429e0a3
Luan Reis <luanreis2202@gmail.com>
2022-04-02 14:56:58 -0300

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -30,7 +30,15 @@ export class ProfilePage {
         this.customer = response;
         this.getImageIfExists();
       },
-        error => {})};
+        error => {
+          if (error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        })
+    }
+    else{
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
   getImageIfExists(){
